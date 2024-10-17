9f27493bc4d224474240abaa5f2457e1cc031437
karthik <krishnankarthik07@gmail.com>
2024-10-17 10:46:24 +0530

    fix: password input field

--- src/utils/validation/loginValidation.ts
+++ src/utils/validation/loginValidation.ts
@@ -6,7 +6,7 @@ export const loginValidationSchema = Yup.object().shape({
       .required("Password is required")
       .matches(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-        "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+        "invalid password"
       ),
     
   });
