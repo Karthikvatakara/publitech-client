3d9c24df0185511ab84afae4448422c09c71d379
karthik <krishnankarthik07@gmail.com>
2024-10-24 17:19:56 +0530

    livestream issue

--- src/skeleton/LoadingPopUp.tsx
+++ src/skeleton/LoadingPopUp.tsx
@@ -7,6 +7,7 @@ interface LoadingPopupProps {
 const LoadingPopUp: React.FC<LoadingPopupProps> = ({ isLoading }) => {
   if (!isLoading) return null;
 
+  
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
       <div className="w-12 h-12 border-4 border-gray-200 border-t-violet-700 rounded-full animate-spin"></div>
